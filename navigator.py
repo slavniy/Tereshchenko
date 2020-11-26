fea49871c55124aed0f2dd0242665f7a0bad9c47
Viktor <larionov92spb@gmail.com>
2020-11-26 13:27:13 +0300

    Алгоритм вроде работает, но нужно избавиться от огромного количества рекурсивных вызово функции traceback

--- navigator.py
+++ navigator.py
@@ -1,3 +1,52 @@
+from PIL import Image
+
+
+def create_matrix_by_image(name='lab.jpg'):
+    im = Image.open(name)
+    pixels = im.load()  # список с пикселями
+    x, y = im.size  # ширина (x) и высота (y) изображения
+    matrix = []
+    for i in range(y):
+        row = []
+        for j in range(x):
+            r, g, b = pixels[i, j]
+            if r > 122 and g > 122 and b > 122:
+                el = '.'
+            elif r < 122 and g < 122 and b < 122:
+                el = '#'
+            elif r < 122 and g > 122 and b < 122:
+                el = 'a'
+            elif r > 122 and g < 122 and b < 122:
+                el = 'b'
+            row.append(el)
+        matrix.append(row)
+    return matrix
+
+
+def create_image_by_matrix(matrix, name='lab.jpg'):
+    im = Image.open(name)
+    pixels = im.load()  # список с пикселями
+    x, y = im.size  # ширина (x) и высота (y) изображения
+    for i in range(y):
+        for j in range(x):
+            if matrix[i][j] == '$':
+                pixels[i, j] = 0, 255, 0
+    im.save('route.jpg')
+
+
+
+
+def create_matrix_by_file(file_name='labirint.txt'):
+    file = open(file_name)
+    place_map = []
+    for line in file:
+        place_map.append(line.split())
+    return place_map
+
+
+
+
+
 def find_start_points(place_map, step):
     points = set()
     for y in range(len(place_map)):
@@ -6,7 +55,8 @@ def find_start_points(place_map, step):
                 points.add((y, x))
     return points
 
-def traceback(row,col, step):
+
+def traceback(row, col, step):
     place_map[row][col] = '$'
     step -= 1
     if row - 1 >= 0 and place_map[row - 1][col] == step:
@@ -15,25 +65,21 @@ def traceback(row,col, step):
         traceback(row + 1, col, step)
     elif col - 1 >= 0 and place_map[row][col - 1] == step:
         traceback(row, col - 1, step)
-
     elif col + 1 < len(place_map[0]) and place_map[row][col + 1] == step:
         traceback(row, col + 1, step)
 
 
-def print_map():
-    for row in place_map:
+def print_map(matrix):
+    for row in matrix:
         print(*row, sep='\t')
     print('-' * 80)
 
 
-file = open('labirint.txt')
-place_map = []
-for line in file:
-    place_map.append(line.split())
-
+place_map = create_matrix_by_image()
 step = 0
 points = find_start_points(place_map, 'a')
 while points:
+    new_points = set()
     step += 1
     for point in points:
         row, col = point
@@ -42,20 +88,24 @@ while points:
                 traceback(row - 1, col, step)
             else:
                 place_map[row - 1][col] = step
+                new_points.add((row - 1, col))
         if row + 1 < len(place_map) and place_map[row + 1][col] in ['.', 'b']:
             if place_map[row + 1][col] == 'b':
                 traceback(row + 1, col, step)
             else:
                 place_map[row + 1][col] = step
+                new_points.add((row + 1, col))
         if col - 1 >= 0 and place_map[row][col - 1] in ['.', 'b']:
             if place_map[row][col - 1] == 'b':
                 traceback(row, col - 1, step)
             else:
                 place_map[row][col - 1] = step
+                new_points.add((row, col - 1))
         if col + 1 < len(place_map[0]) and place_map[row][col + 1] in ['.', 'b']:
             if place_map[row][col + 1] == 'b':
                 traceback(row, col + 1, step)
             else:
                 place_map[row][col + 1] = step
-    points = find_start_points(place_map, step)
-print_map()
+                new_points.add((row, col + 1))
+    points = new_points
+create_image_by_matrix(place_map)
