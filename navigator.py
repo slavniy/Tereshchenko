5c3a86acc1b0a06b7e6e6005ab4b1c0858ef7043
Viktor <larionov92spb@gmail.com>
2020-11-26 13:53:33 +0300

    Алгоритм работает и строит изображение с маршрутом

--- navigator.py
+++ navigator.py
@@ -57,16 +57,18 @@ def find_start_points(place_map, step):
 
 
 def traceback(row, col, step):
-    place_map[row][col] = '$'
-    step -= 1
-    if row - 1 >= 0 and place_map[row - 1][col] == step:
-        traceback(row - 1, col, step)
-    elif row + 1 < len(place_map) and place_map[row + 1][col] == step:
-        traceback(row + 1, col, step)
-    elif col - 1 >= 0 and place_map[row][col - 1] == step:
-        traceback(row, col - 1, step)
-    elif col + 1 < len(place_map[0]) and place_map[row][col + 1] == step:
-        traceback(row, col + 1, step)
+    while step >= 1:
+        print(step)
+        place_map[row][col] = '$'
+        step -= 1
+        if row - 1 >= 0 and place_map[row - 1][col] == step:
+            row = row - 1
+        elif row + 1 < len(place_map) and place_map[row + 1][col] == step:
+            row = row + 1
+        elif col - 1 >= 0 and place_map[row][col - 1] == step:
+            col = col - 1
+        elif col + 1 < len(place_map[0]) and place_map[row][col + 1] == step:
+            col = col + 1
 
 
 def print_map(matrix):
